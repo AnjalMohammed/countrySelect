4b2d61008d88938da4dbf81e38a2e4b6d6bfa033
anjal4CH <manjal@vmware.com>
2024-09-16 06:51:40 +0000

    updating dropdownIndicator

--- src/Components/DropdownIndicator.tsx
+++ src/Components/DropdownIndicator.tsx
@@ -1,12 +1,13 @@
 import { components, DropdownIndicatorProps } from "react-select";
 import { ListOption } from "../data/interfaces";
+import styles from "../styles.module.css";
 
 export function DropdownIndicator(
   props: DropdownIndicatorProps<ListOption, true>
 ) {
   return (
     <components.DropdownIndicator {...props}>
-      <span>{"\u25BC"}</span>
+      <span className={styles.down_caret}>&#9660;</span>
     </components.DropdownIndicator>
   );
 }
